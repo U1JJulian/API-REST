96e8a4e4958ed4fb4dcdd234c7c93d91218a234c
U1JJulian <03jjulianu1@gmail.com>
2025-09-04 19:32:18 -0500

    3_ExpressRouter-CORS
    Implementacion de cors, instalacion y creacion de la vista para pruebas

--- PARCIAL 1/3_ExpressRouter/index.js
+++ PARCIAL 1/3_ExpressRouter/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import routerFrutas from './rutas/frutas.js'
+import cors from 'cors' //npm install cors
 const app = express()
 const PORT = 3000
 
+app.use(cors()) //agregamos cors a nuestra app
 app.use('/frutas', routerFrutas);
 
 app.listen(PORT, () => {
