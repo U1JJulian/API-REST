e8e922ba6325aa4debaddede852cbc4e124559ad
U1JJulian <03jjulianu1@gmail.com>
2025-09-09 08:09:30 -0500

    3_ExpressRouter
    Implementacion de morgan

--- PARCIAL 1/3_ExpressRouter/index.js
+++ PARCIAL 1/3_ExpressRouter/index.js
@@ -1,13 +1,31 @@
 import express from 'express'
 import routerFrutas from './rutas/frutas.js'
-import cors from 'cors' //npm install cors
+import cors from 'cors'
+import morgan from 'morgan'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 const PORT = 3000
 
-//app.use(cors()) //agregamos cors a nuestra app
+// Creamos el stream para logs
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs/access.log'),
+  { flags: 'a' }
+)
 
-app.use('/frutas', routerFrutas);
+// Middlewares
+app.use(cors())
+app.use(morgan('combined', { stream: accessLogStream }))
 
+// Rutas
+app.use('/frutas', routerFrutas)
+
+// Servidor
 app.listen(PORT, () => {
-    console.log(`server listening on port http://localhost:${PORT}`)
-});
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
