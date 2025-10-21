2dfc2366fda11715aaef3750d5180c4d25b60de7
U1JJulian <03jjulianu1@gmail.com>
2025-10-21 08:51:12 -0500

    8_JSDOC
    Implementación de JsDoc

---
+++ PARCIAL 2/8_JSDOC/index.js
@@ -0,0 +1,9 @@
+// Importamos las funciones desde src/calculadora.js
+import { sumar, restar } from './src/calculadora.js';
+
+console.log('Suma:', sumar(8, 2));   // 10
+console.log('Resta:', restar(8, 2)); // 6
+
+
+
+//npm run doc
---
+++ PARCIAL 2/8_JSDOC/src/calculadora.js
@@ -0,0 +1,26 @@
+/**
+ * Suma dos numeros.
+ * @author Jose Julian Gonzalez Medrano 
+ * @param {number} a - Primer numero a sumar.
+ * @param {number} b - Segundo numero a sumar.
+ * @returns {number} El resultado de la suma.
+ * @example
+ * const resultado = sumar(4, 6);
+ * console.log(resultado); // 10
+ */
+
+export function sumar(a, b) {
+  return a + b;
+}
+
+/**
+ * Resta dos numeros.
+ * @author Jose Julian Gonzalez Medrano 
+ * @param {number} a - Minuendo.
+ * @param {number} b - Sustraendo.
+ * @returns {number} El resultado de la resta.
+ */
+
+export function restar(a, b) {
+  return a - b;
+}
